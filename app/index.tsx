de855c986a380b690460ac7c0dea835edaf8e22d
sudhanshugupta-dev <sudhanshugupta-dev>
2025-11-13 11:27:03 +0530

    resolved  lint, update pr-check

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
-           import React from 'react'
+import React from 'react'
 
-const index = () =>                          {
+const index = () => {
   return (
     <View>
       <Text>index</Text>
@@ -9,6 +9,6 @@ const index = () =>                          {
   )
 }
 
-export default                                index
+export default index
 
-const styles =                          StyleSheet.create({})
+const styles = StyleSheet.create({})
