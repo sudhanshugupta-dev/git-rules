aeeb681c31a3bf1615a4ce41f08a47e0fecb7bd7
sudhanshugupta-dev <sudhanshugupta@bitcot.com>
2025-11-14 12:03:45 +0530

    Feature/test button (#12)
    Co-authored-by: sudhanshugupta-dev <sudhanshugupta-dev>

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|react-native-.*|@react-native-community|@react-navigation)',
+  ],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'App.tsx',
+    'src/**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/__tests__/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+};
