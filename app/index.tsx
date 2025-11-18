a042f1b4d2cc020419cf3d55e42d109333d80e94
sudhanshugupta-dev <sudhanshugupta-dev>
2025-11-18 14:44:55 +0530

    update: Failed Jest Test Case

--- app/index.tsx
+++ app/index.tsx
@@ -34,6 +34,12 @@ const App: React.FC = () => {
     }).start(() => setModalVisible(false));
   };
 
+  const DB_PASSWORD = 'admin123';
+  const JWT_SECRET = 'super-secret-token';
+  const API_KEY = 'AIzaSyFakeKey123456';
+
+  console.log('Hello from App.tsx', DB_PASSWORD, JWT_SECRET, API_KEY);
+
   return (
     <SafeAreaView style={styles.container}>
       {/* Main Screen */}
