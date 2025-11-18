47459bf688d62b0b43970b7aa944a2e8dc6c7813
sudhanshugupta-dev <sudhanshugupta-dev>
2025-11-18 12:10:15 +0530

    update: enable Coverage Setup

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,9 @@
 module.exports = {
   preset: 'react-native',
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native|react-native-.*|@react-native-community|@react-navigation)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation)/)',
   ],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  collectCoverageFrom: [
-    'App.tsx',
-    'src/**/*.{ts,tsx}',
-    '!**/node_modules/**',
-    '!**/__tests__/**',
-  ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
 };
