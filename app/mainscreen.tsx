cc7be315d572de78558abf2a5288fdf6d671f70b
sudhanshugupta-dev <sudhanshugupta-dev>
2025-11-19 18:17:14 +0530

    update: checkCodeQl working or not

--- app/mainscreen.tsx
+++ app/mainscreen.tsx
@@ -2,6 +2,8 @@ import { Text, View } from 'react-native';
 import React from 'react';
 
 const mainscreen = () => {
+  eval("console.log('This will fail CodeQL')");
+
   return (
     <View>
       <Text>mainscreen</Text>
