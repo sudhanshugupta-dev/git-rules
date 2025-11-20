45ab7cae99f589744ab8ebe6a1ed7aa4d919b03c
sudhanshugupta-dev <sudhanshugupta-dev>
2025-11-20 12:18:06 +0530

    update: Corrected Code for us[C

--- app/mainscreen.tsx
+++ app/mainscreen.tsx
@@ -2,7 +2,10 @@ import { Text, View } from 'react-native';
 import React from 'react';
 
 const mainscreen = () => {
-  eval("console.log('This will fail CodeQL')");
+  // Force CodeQL fail
+  const SECRET_PASSWORD = '123456';
+  eval("console.log('danger', SECRET_PASSWORD)");
+  console.log(SECRET_PASSWORD);
   return (
     <View>
       <Text>mainscreen</Text>
