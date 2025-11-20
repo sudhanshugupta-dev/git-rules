d0332906b6f862c99f5ca8272372d9903b1ae467
sudhanshugupta-dev <sudhanshugupta-dev>
2025-11-20 17:53:28 +0530

    update: CorrectUses

--- app/mainscreen.tsx
+++ app/mainscreen.tsx
@@ -3,9 +3,6 @@ import React from 'react';
 
 const mainscreen = () => {
   // Force CodeQL fail
-  const SECRET_PASSWORD = '123456';
-  eval("console.log('danger', SECRET_PASSWORD)");
-  console.log(SECRET_PASSWORD);
   return (
     <View>
       <Text>mainscreen</Text>
