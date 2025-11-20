380e38464ee93783722584983b9c536413c4b91c
sudhanshugupta-dev <sudhanshugupta-dev>
2025-11-20 11:08:02 +0530

    update: CodeQlTest

--- app/mainscreen.tsx
+++ app/mainscreen.tsx
@@ -2,6 +2,7 @@ import { Text, View } from 'react-native';
 import React from 'react';
 
 const mainscreen = () => {
+  eval("console.log('This will fail CodeQL')");
   return (
     <View>
       <Text>mainscreen</Text>
