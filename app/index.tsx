a7108c3f2e7b039ac112df926804367861d9eac0
sudhanshugupta-dev <sudhanshugupta-dev>
2025-11-26 18:11:32 +0530

    update: with meta data

--- app/index.tsx
+++ app/index.tsx
@@ -36,11 +36,11 @@ const App: React.FC = () => {
   };
   console.log('Hello from index.tsx');
 
-  // const DB_PASSWORD = 'admin123';
-  // const JWT_SECRET = 'super-secret-token';
-  // const API_KEY = 'AIzaSyFakeKey123456';
+  const DB_PASSWORD = 'admin123';
+  const JWT_SECRET = 'super-secret-token';
+  const API_KEY = 'AIzaSyFakeKey123456';
 
-  // console.log('Hello from App.tsx', DB_PASSWORD, JWT_SECRET, API_KEY);
+  console.log('Hello from App.tsx', DB_PASSWORD, JWT_SECRET, API_KEY);
 
   return (
     <SafeAreaView style={styles.container}>
